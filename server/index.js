dc1973096ae2717ff1fad19d9bcf7be84fcc1689
Jonathan Bradshaw <jonathan.lavin.bradshaw@gmail.com>
2017-10-17 18:48:52 -0700

    (fix) Load Angular Node module in Heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 app.use(express.static(path.join(__dirname, '../public')));
 
